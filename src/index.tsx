2a7bb1e2135da7ea8b2733c3d0dee77ddde56ed4
Shah Md. Shafi Imam <71667417+shafiimam@users.noreply.github.com>
2023-03-19 22:55:05 +0600

    scale canvas

--- src/index.tsx
+++ src/index.tsx
@@ -57,10 +57,17 @@ class Scratch extends Component<Props, State> {
   }
 
   componentDidMount() {
+    console.log('this is', this);
     this.isDrawing = false;
     this.lastPoint = null;
     this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
-
+    this.canvas.width = Math.floor(this.props.width * window.devicePixelRatio);
+    this.canvas.height = Math.floor(this.props.height * window.devicePixelRatio);
+    console.log('canvas h/w current', this.canvas.height, this.canvas.width);
+    this.ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+    this.canvas.style.width = this.props.width + 'px';
+    this.canvas.style.height = this.props.height + 'px';
+    console.log('canva h/w updated', this.canvas.style.height, this.canvas.style.width);
     this.image = new Image();
     this.image.crossOrigin = 'Anonymous';
     this.image.onload = () => {
